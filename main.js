4a6b481f6f796e1aa079a0b07522e8378f5965ec
Ajinaline <otokpa@my.yorku.ca>
2025-05-09 01:45:40 -0400

    Added js
    Added js for the color of the languages i know

--- main.js
+++ main.js
@@ -4,4 +4,13 @@
 setInterval('autoRefresh()', 1000);*/
 const colors = ["#AE445A", "#E8BCB9", "#4B4376", "#A64D79", "#006A67", "#C63C51", "#EB3678"];
 
-document.getElementById("language").style.backgroundColor = colors[Math.floor(Math.random() * 7)];
+var programcolor = document.getElementsByClassName("language");
+
+if (!programcolor){
+    console.log("Not working");
+}
+else{
+    for (let i = 0; i < programcolor.length; i++){
+        programcolor[i].style.backgroundColor = colors[Math.floor(Math.random()*7)];
+    }
+}
