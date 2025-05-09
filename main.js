e2bbe43c4c4d66b12d20f957f7ec2693c5308460
Ajinaline <otokpa@my.yorku.ca>
2025-05-09 00:50:00 -0400

    Color changes
    Some color changes and setup to upcoming changes

--- main.js
+++ main.js
@@ -2,3 +2,6 @@
     window.location = window.location.href;
 }
 setInterval('autoRefresh()', 1000);*/
+const colors = ["#AE445A", "#E8BCB9", "#4B4376", "#A64D79", "#006A67", "#C63C51", "#EB3678"];
+
+document.getElementById("language").style.backgroundColor = colors[Math.floor(Math.random() * 7)];
