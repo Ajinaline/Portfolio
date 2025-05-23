96e2f653b6025b116b98e6f2d3429df718a60185
Ajinaline <otokpa@my.yorku.ca>
2025-05-23 01:35:48 -0400

    Redesign

--- main.js
+++ main.js
@@ -2,15 +2,7 @@
     window.location = window.location.href;
 }
 setInterval('autoRefresh()', 1000);*/
-const colors = ["#AE445A", "#E8BCB9", "#4B4376", "#A64D79", "#006A67", "#C63C51", "#EB3678"];
+const colors = ["React Javascript", ""];
 
 var programcolor = document.getElementsByClassName("language");
 
-if (!programcolor){
-    console.log("Not working");
-}
-else{
-    for (let i = 0; i < programcolor.length; i++){
-        programcolor[i].style.backgroundColor = colors[Math.floor(Math.random()*7)];
-    }
-}
