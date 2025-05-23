c52bb16a356bf9be3836563ec2f49073eec320da
Ajinaline <otokpa@my.yorku.ca>
2025-05-23 02:20:06 -0400

    Added Javascript for languages and frameworks

--- main.js
+++ main.js
@@ -2,7 +2,18 @@
     window.location = window.location.href;
 }
 setInterval('autoRefresh()', 1000);*/
-const colors = ["React Javascript", ""];
+const languages = ["React Javascript", "React TypeScript", "Python", "Java", "Kotlin"];
 
-var programcolor = document.getElementsByClassName("language");
+var space = document.getElementById("language");
 
+for (let i = 0; i < languages.length; i++){
+    space.innerHTML += `<a> ${languages[i]} </a>`;
+}
+
+const frameworks = ["Github", "React TypeScript", "Python", "Java", "Kotlin"];
+
+var space = document.getElementById("framework");
+
+for (let i = 0; i < languages.length; i++){
+    space.innerHTML += `<a> ${languages[i]} </a>`;
+}
