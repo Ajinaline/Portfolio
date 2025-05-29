3e3e73c96b9becc2cc75b06309ab1e2d53850eeb
Ajinaline <otokpa@my.yorku.ca>
2025-05-28 23:49:41 -0400

    Fixed the timeline
    Added the template for my job history

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
     window.location = window.location.href;
 }
 setInterval('autoRefresh()', 1000);*/
-const languages = ["React Javascript", "React TypeScript", "Python", "Java", "Kotlin"];
+
+//This is the code for the languages used
+/*const languages = ["HTML", "CSS", "JavaScript", "React.js", "React.ts", "Python", "Java", "Kotlin"];
 
 var space = document.getElementById("language");
 
@@ -10,10 +12,13 @@ for (let i = 0; i < languages.length; i++){
     space.innerHTML += `<a> ${languages[i]} </a>`;
 }
 
-const frameworks = ["Github", "React TypeScript", "Python", "Java", "Kotlin"];
+
+//This is the code for the frameworks used
+const frameworks = ["Github", "Postman", "Kali Linux", "VScode", "Android Studio", ""];
 
 var space = document.getElementById("framework");
 
 for (let i = 0; i < languages.length; i++){
     space.innerHTML += `<a> ${languages[i]} </a>`;
 }
+    */
